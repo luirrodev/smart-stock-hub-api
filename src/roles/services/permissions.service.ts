e0a04b78f1cf32c0447074dd670c6335cde1031d
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2025-04-30 14:46:01 -0400

    Add PermissionsService for managing permissions with CRUD operations

---
+++ src/roles/services/permissions.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Permission } from '../entities/permission.entity';
+import { Repository } from 'typeorm';
+import {
+  CreatePermissionDto,
+  UpdatePermissionDto,
+} from '../dtos/permission.dto';
+
+@Injectable()
+export class PermissionsService {
+  constructor(
+    @InjectRepository(Permission)
+    private permissionRepo: Repository<Permission>,
+  ) {}
+
+  async getAllPermissions() {
+    return this.permissionRepo.find();
+  }
+
+  async getPermissionById(id: number) {
+    const permission = await this.permissionRepo.findOne({
+      where: { id },
+    });
+    if (!permission) {
+      throw new Error('Permission not found');
+    }
+    return permission;
+  }
+
+  async createPermission(data: CreatePermissionDto) {
+    const permission = this.permissionRepo.create(data);
+    return this.permissionRepo.save(permission);
+  }
+
+  async updatePermission(id: number, data: UpdatePermissionDto) {
+    const permission = await this.getPermissionById(id);
+    permission.description = data.description;
+    return this.permissionRepo.save(permission);
+  }
+}
