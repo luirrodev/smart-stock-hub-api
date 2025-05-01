84756f6a93755964238d04ef8caef6ba2f2137b0
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2025-05-01 16:31:06 -0400

    Refactor PermissionsGuard to streamline role and permission checks

--- src/roles/guards/permissions.guard.ts
+++ src/roles/guards/permissions.guard.ts
@@ -2,6 +2,7 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { REQUIRE_PERMISSIONS_KEY } from '../decorators/permissions.decorator';
 import { ROLES_KEY } from '../decorators/roles.decorator';
+import { PayloadToken } from 'src/auth/models/token.model';
 
 @Injectable()
 export class PermissionsGuard implements CanActivate {
@@ -18,23 +19,19 @@ export class PermissionsGuard implements CanActivate {
       context.getHandler(),
     );
 
-    // 2. Si no hay restricciones, permitir acceso
-    if (!requiredPermissions && !requiredRoles) return true;
-
     // 3. Obtener el usuario del request (asumiendo JWT)
     const request = context.switchToHttp().getRequest();
-    const user = request.user;
+    const user = request.user as PayloadToken;
 
     // 4. Verificar roles (si hay)
-    if (requiredRoles && !requiredRoles.includes(user.role.name)) {
+    if (requiredRoles && !requiredRoles.includes(user.role)) {
       return false;
     }
 
     // 5. Verificar permisos (si hay)
     if (requiredPermissions) {
-      const userPermissions = user.role.permissions.map((p) => p.name);
       const hasPermission = requiredPermissions.some((p) =>
-        userPermissions.includes(p),
+        user.permissions.includes(p),
       );
       if (!hasPermission) return false;
     }
