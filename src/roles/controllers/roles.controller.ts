2a452fcdd0bde805499a8e9dd840626c4530a56b
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2025-07-20 02:06:52 -0400

    Implementa autenticación y autorización en los controladores de roles y permisos: se añaden los decoradores de roles y permisos, y se aplica el guardia JWTAuthGuard para proteger las rutas. Se actualizan los métodos para requerir permisos específicos según la acción realizada.

--- src/roles/controllers/roles.controller.ts
+++ src/roles/controllers/roles.controller.ts
@@ -12,31 +12,40 @@ import {
 
 import { RolesService } from '../services/roles.service';
 import { AssignPermissionsDto, CreateRoleDto } from '../dtos/role.dto';
-// import { AssignPermissionsDto } from './dto/assign-permissions.dto';
-// import { RequirePermissions } from '../decorators/permissions.decorator';
 import { PermissionsGuard } from '../guards/permissions.guard';
+import { JWTAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Roles } from '../decorators/roles.decorator';
+import { RequirePermissions } from '../decorators/permissions.decorator';
 
 @Controller('roles')
-@UseGuards(PermissionsGuard)
+@UseGuards(JWTAuthGuard, PermissionsGuard)
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
   @Get()
+  @Roles('admin')
+  @RequirePermissions('roles:read')
   getAllRoles() {
     return this.rolesService.getAllRoles();
   }
 
   @Get(':id')
+  @Roles('admin')
+  @RequirePermissions('roles:read')
   getRoleById(@Param('id', ParseIntPipe) id: number) {
     return this.rolesService.getRoleById(id);
   }
 
   @Post()
+  @Roles('admin')
+  @RequirePermissions('roles:write')
   createRole(@Body() payload: CreateRoleDto) {
     return this.rolesService.createRole(payload);
   }
 
   @Post(':id/permissions')
+  @Roles('admin')
+  @RequirePermissions('roles:write')
   assignPermissions(
     @Param('id', ParseIntPipe) id: number,
     @Body() payload: AssignPermissionsDto,
@@ -45,11 +54,15 @@ export class RolesController {
   }
 
   @Put(':id')
+  @Roles('admin')
+  @RequirePermissions('roles:write')
   updateRole(@Param('id', ParseIntPipe) id: number, @Body() payload) {
     return this.rolesService.updateRole(id, payload);
   }
 
   @Delete(':id')
+  @Roles('admin')
+  @RequirePermissions('roles:write')
   deleteRole(@Param('id', ParseIntPipe) id: number) {
     return this.rolesService.deleteRole(id);
   }
