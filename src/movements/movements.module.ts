12571b1d6a4d2735992e803b3afbb9b4757624ae
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2025-07-20 19:12:46 -0400

    Agrega servicios 'MovementsService' y 'MovementTypesService' al módulo 'MovementsModule', e integra los módulos 'UsersModule' y 'ProductsModule' para mejorar la gestión de movimientos e interacciones con usuarios y productos.

--- src/movements/movements.module.ts
+++ src/movements/movements.module.ts
@@ -4,6 +4,14 @@ import { MovementType } from './entities/movement-type.entity';
 import { InventoryMovement } from './entities/inventory-movement.entity';
 import { MovementDetail } from './entities/movement-detail.entity';
 import { MovementApproval } from './entities/movement-approval.entity';
+import { MovementsService } from './services/movements.service';
+import { MovementTypesService } from './services/movement-types.service';
+import { UsersService } from 'src/users/services/users.service';
+import { ProductsService } from 'src/products/services/products.service';
+import { InventoryService } from 'src/products/services/inventory.service';
+import { WarehousesService } from 'src/products/services/warehouses.service';
+import { UsersModule } from 'src/users/users.module';
+import { ProductsModule } from 'src/products/products.module';
 
 @Module({
   imports: [
@@ -13,9 +21,11 @@ import { MovementApproval } from './entities/movement-approval.entity';
       MovementDetail,
       MovementApproval,
     ]),
+    UsersModule,
+    ProductsModule,
   ],
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [MovementsService, MovementTypesService],
+  exports: [MovementsService, MovementTypesService],
 })
 export class MovementsModule {}
