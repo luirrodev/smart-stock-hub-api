2cc3f0f3c79a8eb9498bfe5aa3cfb078cfc27e5a
Luis Ramon Diaz <luisr@allnovu.com>
2026-01-19 13:42:10 -0500

    Elimina el decorador de Roles en RolesController para simplificar la gestión de permisos

--- src/access-control/roles/controllers/roles.controller.ts
+++ src/access-control/roles/controllers/roles.controller.ts
@@ -14,7 +14,6 @@ import { RolesService } from '../services/roles.service';
 import { AssignPermissionsDto, CreateRoleDto } from '../dtos/role.dto';
 import { PermissionsGuard } from '../../permissions/guards/permissions.guard';
 import { JWTAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { Roles } from '../decorators/roles.decorator';
 import { RequirePermissions } from '../../permissions/decorators/permissions.decorator';
 
 @Controller('roles')
@@ -23,28 +22,24 @@ export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
   @Get()
-  @Roles('admin')
   @RequirePermissions('roles:read')
   getAllRoles() {
     return this.rolesService.getAllRoles();
   }
 
   @Get(':id')
-  @Roles('admin')
   @RequirePermissions('roles:read')
   getRoleById(@Param('id', ParseIntPipe) id: number) {
     return this.rolesService.getRoleById(id);
   }
 
   @Post()
-  @Roles('admin')
   @RequirePermissions('roles:write')
   createRole(@Body() payload: CreateRoleDto) {
     return this.rolesService.createRole(payload);
   }
 
   @Post(':id/permissions')
-  @Roles('admin')
   @RequirePermissions('roles:write')
   assignPermissions(
     @Param('id', ParseIntPipe) id: number,
@@ -54,14 +49,12 @@ export class RolesController {
   }
 
   @Put(':id')
-  @Roles('admin')
   @RequirePermissions('roles:write')
   updateRole(@Param('id', ParseIntPipe) id: number, @Body() payload) {
     return this.rolesService.updateRole(id, payload);
   }
 
   @Delete(':id')
-  @Roles('admin')
   @RequirePermissions('roles:write')
   deleteRole(@Param('id', ParseIntPipe) id: number) {
     return this.rolesService.deleteRole(id);
