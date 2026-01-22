393576942c8d06ddff96e03c8a765dbffba0c2f3
Luis Ramon Diaz <luisr@allnovu.com>
2026-01-22 13:22:28 -0500

    Elimina el controlador CustomersController del módulo Customers para simplificar la estructura del módulo

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Customer } from './entities/customer.entity';
-import { CustomersController } from './controllers/customers.controller';
 import { CustomersService } from './services/customers.service';
 import { AccessControlModule } from '../access-control/access-control.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Customer]), AccessControlModule],
-  controllers: [CustomersController],
+  controllers: [],
   providers: [CustomersService],
   exports: [CustomersService],
 })
