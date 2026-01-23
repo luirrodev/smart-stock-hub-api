25929c13d012c5ff97c908b4808b80549368c3dd
Luis Ramon Diaz <luisr@allnovu.com>
2026-01-23 15:40:01 -0500

    Agrega HttpModule y ProductsService al módulo ProductsModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
 
 import { Product } from './entities/product.entity';
 import { AccessControlModule } from '../access-control/access-control.module';
+import { ProductsService } from './services/products.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product]), AccessControlModule],
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    AccessControlModule,
+    HttpModule,
+  ],
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [ProductsService],
+  exports: [ProductsService],
 })
 export class ProductsModule {}
