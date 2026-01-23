7674d987c81c60d953d6237b95ebbfed98987aa8
Luis Ramon Diaz <luisr@allnovu.com>
2026-01-23 15:18:14 -0500

    Elimina los controladores y servicios del módulo ProductsModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Product } from './entities/product.entity';
-import { ProductsService } from './services/products.service';
-import { ProductsController } from './controllers/products.controller';
 import { AccessControlModule } from '../access-control/access-control.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product]), AccessControlModule],
-  controllers: [ProductsController],
-  providers: [ProductsService],
-  exports: [ProductsService],
+  controllers: [],
+  providers: [],
+  exports: [],
 })
 export class ProductsModule {}
