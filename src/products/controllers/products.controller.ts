35bd0237e4db19e510b5e9caa2489e79aa3234ee
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2026-01-25 12:14:36 -0500

    Agrega decoradores de Swagger para la respuesta paginada en el endpoint de obtención de productos

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -7,17 +7,24 @@ import {
   Get,
   Query,
 } from '@nestjs/common';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiOkResponse,
+  ApiExtraModels,
+} from '@nestjs/swagger';
 
 import { ProductsService } from '../services/products.service';
-import { PaginationDto } from 'src/common/dtos/pagination.dto';
+
+import { ProductPaginatedResponse } from '../dtos/product-paginated-response.dto';
+import { ProductPaginationDto } from '../dtos/product-pagination.dto';
 
 import { PermissionsGuard } from 'src/access-control/permissions/guards/permissions.guard';
 import { JWTAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RequirePermissions } from 'src/access-control/permissions/decorators/permissions.decorator';
-import { ProductPaginationDto } from '../dtos/product-pagination.dto';
 
 @ApiTags('products')
+@ApiExtraModels(ProductPaginatedResponse)
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
@@ -35,6 +42,10 @@ export class ProductsController {
 
   @Get()
   @ApiOperation({ summary: 'Obtener todos los productos paginados' })
+  @ApiOkResponse({
+    description: 'Respuesta paginada de productos',
+    type: ProductPaginatedResponse,
+  })
   async getAll(@Query() query: ProductPaginationDto) {
     return await this.productsService.getAllProducts(query);
   }
