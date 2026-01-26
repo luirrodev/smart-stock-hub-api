b31dcfb5de504f3ca96d5f3bba58d19d3128e1f8
Luis Ramon Diaz <luisr@allnovu.com>
2026-01-26 08:36:08 -0500

    Agrega CartItem al módulo de carritos para gestionar los elementos del carrito

--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cart } from './entities/cart.entity';
 import { CartsService } from './services/carts.service';
 import { CartsController } from './controllers/carts.controller';
+import { CartItem } from './entities/cart-item.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cart])],
+  imports: [TypeOrmModule.forFeature([Cart, CartItem])],
   controllers: [CartsController],
   providers: [CartsService],
   exports: [CartsService],
