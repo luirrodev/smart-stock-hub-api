971c7a636d5e9c639192b12bb957a5512361fc92
Luis Ramon Diaz <luisr@allnovu.com>
2026-01-26 09:08:26 -0500

    Agrega el módulo ProductsModule a las importaciones del CartsModule

--- src/carts/carts.module.ts
+++ src/carts/carts.module.ts
@@ -2,14 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Cart } from './entities/cart.entity';
-import { CartsService } from './services/carts.service';
+import { CartService } from './services/carts.service';
 import { CartsController } from './controllers/carts.controller';
 import { CartItem } from './entities/cart-item.entity';
+import { ProductsModule } from '../products/products.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cart, CartItem])],
+  imports: [TypeOrmModule.forFeature([Cart, CartItem]), ProductsModule],
   controllers: [CartsController],
-  providers: [CartsService],
-  exports: [CartsService],
+  providers: [CartService],
+  exports: [CartService],
 })
 export class CartsModule {}
