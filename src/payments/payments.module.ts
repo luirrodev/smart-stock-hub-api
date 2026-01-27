dfb2c17cf0998fbc2440aff96fb42ea4040d1aff
Luis Ramon Diaz <luisr@allnovu.com>
2026-01-27 15:38:47 -0500

    Optimiza la importación de módulos en PaymentsModule y elimina la importación innecesaria de Store

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -4,22 +4,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Payment } from './entities/payment.entity';
 import { StorePaymentConfig } from './entities/store-payment-config.entity';
 import { PaymentTransaction } from './entities/payment-transaction.entity';
-import { Store } from '../stores/entities/store.entity';
 import { StoresModule } from '../stores/stores.module';
 import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { PaypalService } from './providers/paypal/paypal.service';
 import { PaypalWebhookController } from './providers/paypal/paypal.webhook.controller';
+import { OrdersModule } from 'src/orders/orders.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      StorePaymentConfig,
-      Payment,
-      PaymentTransaction,
-      Store,
-    ]),
+    TypeOrmModule.forFeature([StorePaymentConfig, Payment, PaymentTransaction]),
     StoresModule,
+    OrdersModule,
   ],
   controllers: [PaymentsController, PaypalWebhookController],
   providers: [PaymentsService, PaypalService],
