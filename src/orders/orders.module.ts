379746375b0304b892f4e1d44fc252063db896d8
Luis Ramon Diaz <luisr@allnovu.com>
2026-01-27 10:04:14 -0500

    Añade OrderItem y Product al módulo de pedidos

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,13 +2,24 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order-items.entity';
 import { PickupPoint } from './entities/pickup-point.entity';
 import { OrderStatus } from './entities/order-status.entity';
+import { Product } from '../products/entities/product.entity';
+import { OrdersService } from './services/orders.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, PickupPoint, OrderStatus])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Order,
+      OrderItem,
+      PickupPoint,
+      OrderStatus,
+      Product,
+    ]),
+  ],
   controllers: [],
-  providers: [],
-  exports: [],
+  providers: [OrdersService],
+  exports: [OrdersService],
 })
 export class OrdersModule {}
