26916b6597cace3b3394b075c3982404c7978f9c
Luis Ramon Diaz <luisr@allnovu.com>
2026-01-28 12:13:27 -0500

    Añade imports y configuración de módulos necesarios para el manejo de webhooks de PayPal y Stripe

--- src/webhooks/webhooks.module.ts
+++ src/webhooks/webhooks.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { PaypalWebhookController } from './paypal/paypal-webhook.controller';
 import { PaypalWebhookService } from './paypal/paypal-webhook.service';
 import { StripeWebhookController } from './stripe/stripe-webhook.controller';
 import { StripeWebhookService } from './stripe/stripe-webhook.service';
+import { Payment } from 'src/payments/entities/payment.entity';
+import { StorePaymentConfig } from 'src/payments/entities/store-payment-config.entity';
+import { PaymentsModule } from 'src/payments/payments.module';
 
 @Module({
+  imports: [
+    HttpModule,
+    ConfigModule,
+    TypeOrmModule.forFeature([Payment, StorePaymentConfig]),
+    PaymentsModule,
+  ],
   controllers: [PaypalWebhookController, StripeWebhookController],
   providers: [PaypalWebhookService, StripeWebhookService],
   exports: [PaypalWebhookService, StripeWebhookService],
