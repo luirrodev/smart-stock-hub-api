94a1c4a71db8e23abdf275e1b40c8be0a25706bd
Luis Ramón Díaz <diazluisramon.dev@gmail.com>
2026-01-29 12:21:42 -0500

    Agregar propiedad provider a CreatePaymentDto y mejorar la validación

--- src/payments/dto/create-payment.dto.ts
+++ src/payments/dto/create-payment.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsPositive } from 'class-validator';
+import { IsEnum, IsInt, IsPositive } from 'class-validator';
+import { PaymentProvider } from '../entities/store-payment-config.entity';
 
 export class CreatePaymentDto {
   @ApiProperty({ example: 123, description: 'ID de la orden a pagar' })
   @IsInt()
   @IsPositive()
   orderId: number;
+
+  @ApiProperty({ example: PaymentProvider.PAYPAL })
+  @IsEnum(PaymentProvider)
+  provider: PaymentProvider;
 }
