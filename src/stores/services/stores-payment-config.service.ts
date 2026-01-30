d575d3e7814f34376410f25a684e7515da311cb2
Luis Ramón Díaz <diazluisramon.dev@gmail.com>
2026-01-30 10:23:27 -0500

    Agregar StoresPaymentConfigService y su repositorio en StoresModule

--- src/stores/services/stores-payment-config.service.ts
+++ src/stores/services/stores-payment-config.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm/repository/Repository';
+
+import { StorePaymentConfig } from '../entities/store-payment-config.entity';
 
 @Injectable()
-export class StoresPaymentConfigService {}
+export class StoresPaymentConfigService {
+  constructor(
+    @InjectRepository(StorePaymentConfig)
+    private readonly storePaymentConfigRepo: Repository<StorePaymentConfig>,
+  ) {}
+}
--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Store } from './entities/store.entity';
+import { StorePaymentConfig } from './entities/store-payment-config.entity';
+
 import { StoresService } from './services/stores.service';
-import { StoresController } from './controllers/stores.controller';
 import { StoresPaymentConfigService } from './services/stores-payment-config.service';
 
+import { StoresController } from './controllers/stores.controller';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Store])],
+  imports: [TypeOrmModule.forFeature([Store, StorePaymentConfig])],
   controllers: [StoresController],
   providers: [StoresService, StoresPaymentConfigService],
   exports: [StoresService],
