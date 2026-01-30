fe0c44e93c3c48e368b72a7671fbab36d3c0190a
Luis Ramón Díaz <diazluisramon.dev@gmail.com>
2026-01-30 10:11:34 -0500

    Agregar StoresPaymentConfigService y registrarlo en StoresModule

--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -4,11 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Store } from './entities/store.entity';
 import { StoresService } from './services/stores.service';
 import { StoresController } from './controllers/stores.controller';
+import { StoresPaymentConfigService } from './services/stores-payment-config.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Store])],
   controllers: [StoresController],
-  providers: [StoresService],
+  providers: [StoresService, StoresPaymentConfigService],
   exports: [StoresService],
 })
 export class StoresModule {}
