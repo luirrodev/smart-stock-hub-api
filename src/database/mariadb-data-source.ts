9dc250ca23160e1a3ca7df75fd178c955653dff8
Luis Ramón Díaz <diazluisramon.dev@gmail.com>
2026-02-16 10:49:09 -0500

    feat(database): actualizar configuración de conexión a MariaDB para usar 127.0.0.1 y establecer valores predeterminados para usuario y contraseña

--- src/database/mariadb-data-source.ts
+++ src/database/mariadb-data-source.ts
@@ -11,11 +11,14 @@ const isProd = process.env.NODE_ENV === 'prod';
  */
 export const MariaDbDataSource = new DataSource({
   type: 'mariadb',
-  host: process.env.MARIADB_HOST || 'localhost',
+  host: process.env.MARIADB_HOST || '127.0.0.1', // Usar 127.0.0.1 en lugar de localhost para IPv4
   port: parseInt(process.env.MARIADB_PORT || '3306'),
-  username: process.env.MARIADB_USER,
-  password: process.env.MARIADB_PASSWORD,
+  username: process.env.MARIADB_USER || 'root',
+  password: process.env.MARIADB_PASSWORD || '',
   database: process.env.MARIADB_DATABASE,
   synchronize: false,
   logging: !isProd,
+  connectTimeout: 10000, // 10 segundos de timeout
+  supportBigNumbers: true,
+  bigNumberStrings: true,
 });
