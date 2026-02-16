eb1eb92767c0e3532607a3e5d08f87f49854d360
Luis Ramón Díaz <diazluisramon.dev@gmail.com>
2026-02-16 09:29:46 -0500

    feat(inventory): actualizar módulo Inventory para usar ComponentController y ComponentService

--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Component } from './entities/component.entity';
-import { InventoryService } from './services/inventory.service';
-import { InventoryController } from './controllers/inventory.controller';
+import { ComponentService } from './services/component.service';
+import { ComponentController } from './controllers/component.controller';
 import { AccessControlModule } from '../access-control/access-control.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Component]), AccessControlModule],
-  controllers: [InventoryController],
-  providers: [InventoryService],
-  exports: [InventoryService],
+  controllers: [ComponentController],
+  providers: [ComponentService],
+  exports: [ComponentService],
 })
 export class InventoryModule {}
