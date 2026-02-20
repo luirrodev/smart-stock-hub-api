62f80fab2f40035320a82a98b5f8608d6e3f2681
Luis Ramón Díaz <diazluisramon.dev@gmail.com>
2026-02-20 09:51:36 -0500

    fix(payments): corregir rutas de importación y agregar PaymentsV2Controller en PaymentsModule

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -8,9 +8,10 @@ import config from 'src/config';
 import { Payment } from './entities/payment.entity';
 import { PaymentTransaction } from './entities/payment-transaction.entity';
 import { StoresModule } from '../stores/stores.module';
-import { JwtSignatureService } from './jwt-signature.service';
-import { PaymentsService } from './payments.service';
-import { PaymentsController } from './payments.controller';
+import { JwtSignatureService } from './services/jwt-signature.service';
+import { PaymentsService } from './services/payments.service';
+import { PaymentsController } from './controllers/payments-v1.controller';
+import { PaymentsV2Controller } from './controllers/payments-v2.controller';
 import { PaypalService } from './providers/paypal/paypal.service';
 import { OrdersModule } from 'src/orders/orders.module';
 
@@ -27,7 +28,7 @@ import { OrdersModule } from 'src/orders/orders.module';
       }),
     }),
   ],
-  controllers: [PaymentsController],
+  controllers: [PaymentsController, PaymentsV2Controller],
   providers: [PaymentsService, PaypalService, JwtSignatureService],
   exports: [PaymentsService, PaypalService],
 })
