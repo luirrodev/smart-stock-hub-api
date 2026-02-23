575a114c6b41df545a0b6c7dfedb39e1d18dddf6
Luis Ramón Díaz <diazluisramon.dev@gmail.com>
2026-02-23 13:32:39 -0500

    feat(products): actualizar módulo Products para incluir ProductStore y Store en TypeOrmModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { Product } from './entities/product.entity';
+import { ProductStore } from './entities/product-store.entity';
+import { Store } from '../stores/entities/store.entity';
+
 import { ProductsService } from './services/products.service';
+import { ProductStoreService } from './services/product-store.service';
+
 import { ProductsV1Controller, ProductsV2Controller } from './controllers';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product, ProductStore, Store])],
   controllers: [ProductsV1Controller, ProductsV2Controller],
-  providers: [ProductsService],
-  exports: [ProductsService],
+  providers: [ProductsService, ProductStoreService],
+  exports: [ProductsService, ProductStoreService],
 })
 export class ProductsModule {}
