83a54ef5d1b17e29316a3a05c0187bb2610dfd9f
Luis Ramón Díaz <diazluisramon.dev@gmail.com>
2026-02-23 11:25:10 -0500

    feat(mariadb-sync): hacer el módulo global para la sincronización de datos

--- src/database/mariadb-sync.module.ts
+++ src/database/mariadb-sync.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { MariaDbSyncService } from './services/mariadb-sync.service';
 
 /**
  * Módulo para sincronización de datos desde MariaDB a PostgreSQL
  * Proporciona servicios para traer datos de MariaDB de forma simple
  */
+@Global()
 @Module({
   providers: [MariaDbSyncService],
   exports: [MariaDbSyncService],
