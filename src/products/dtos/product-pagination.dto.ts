de0eb0b7efd3fbb85c04fea93db351a5e4182a64
Luis Ramón Díaz <diazluisramon.dev@gmail.com>
2026-02-24 08:42:51 -0500

    refactor(products): utilizar OmitType en ProductPaginationDto para excluir el campo 'search'

--- src/products/dtos/product-pagination.dto.ts
+++ src/products/dtos/product-pagination.dto.ts
@@ -1,7 +1,8 @@
 import { IsOptional, IsIn } from 'class-validator';
+import { OmitType } from '@nestjs/swagger';
 import { PaginationDto } from '../../common/dtos/pagination.dto';
 
-export class ProductPaginationDto extends PaginationDto {
+export class ProductPaginationDto extends OmitType(PaginationDto, ['search']) {
   @IsOptional()
   @IsIn(['id', 'name', 'price'])
   declare sortBy?: 'id' | 'name' | 'price';
