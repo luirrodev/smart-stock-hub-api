5c3ddbd03c3daeb5b1851bf5880893ca2ce8c4fd
Luis Ramón Díaz <diazluisramon.dev@gmail.com>
2026-02-24 14:42:41 -0500

    feat(products): exportar CategoriesV1Controller en el módulo de productos

--- src/products/controllers/index.ts
+++ src/products/controllers/index.ts
@@ -1,2 +1,3 @@
 export { ProductsV1Controller } from './products-v1.controller';
 export { ProductsV2Controller } from './products-v2.controller';
+export { CategoriesV1Controller } from './categories-v1.controller';
