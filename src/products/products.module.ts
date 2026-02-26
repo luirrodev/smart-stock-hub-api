38c2fcb0ec788dbfb46137ec3e9f878f4f865a9c
Luis Ramón Díaz <diazluisramon.dev@gmail.com>
2026-02-26 16:02:39 -0500

    refactor(products): mejorar formato de importaciones y organización de módulos

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -5,13 +5,18 @@ import { Product } from './entities/product.entity';
 import { ProductStore } from './entities/product-store.entity';
 import { Category } from './entities/category.entity';
 import { ProductStoreCategory } from './entities/product-store-category.entity';
+import { ProductStoreImage } from './entities/product-store-image.entity';
 import { Store } from '../stores/entities/store.entity';
 
 import { ProductsService } from './services/products.service';
 import { ProductStoreService } from './services/product-store.service';
 import { CategoryService } from './services/category.service';
 
-import { ProductsV1Controller, ProductsV2Controller, CategoriesV1Controller } from './controllers';
+import {
+  ProductsV1Controller,
+  ProductsV2Controller,
+  CategoriesV1Controller,
+} from './controllers';
 import { StoresModule } from 'src/stores/stores.module';
 
 @Module({
@@ -21,11 +26,16 @@ import { StoresModule } from 'src/stores/stores.module';
       ProductStore,
       Category,
       ProductStoreCategory,
+      ProductStoreImage,
       Store,
     ]),
     StoresModule,
   ],
-  controllers: [ProductsV1Controller, ProductsV2Controller, CategoriesV1Controller],
+  controllers: [
+    ProductsV1Controller,
+    ProductsV2Controller,
+    CategoriesV1Controller,
+  ],
   providers: [ProductsService, ProductStoreService, CategoryService],
   exports: [ProductsService, ProductStoreService, CategoryService],
 })
